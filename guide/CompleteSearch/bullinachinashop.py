f56f0ca4380c7da7d61a5a8591e8b97d2e8dcfaf
ScientificSnake <andrew.segerlind@gmail.com>
2026-03-05 15:28:41 -0800

    more bull in csop

--- guide/CompleteSearch/bullinachinashop.py
+++ guide/CompleteSearch/bullinachinashop.py
@@ -12,59 +12,67 @@ def readNextPiece() -> list:
 
 bull = readNextPiece()
 ups = []
+allpiecesList = {}
 workingShifts = []
 sides = []
-for i in range(1,Kpieces+1): # start at 1
+for piecenum in range(1,Kpieces+1): # start at 1
     mainPiece = readNextPiece()
     workingShifts.clear()
     ups.clear()
-    ups.append(workingShifts)
+    ups.append(mainPiece)
     #up 
     upindex = 0
     piece = mainPiece
     while True:
-        cant = False
-        for i in Nsize:
-            if piece[i]:
-                cant = True
-                break
-        if cant:
+        if any(piece[:4]):
             break
         piece = piece[4:]
         piece.extend([False] * 4)
-        ups.append(piece)
+        ups.append(piece.copy())
     # down
     piece = mainPiece
     while True:
-        cant = False
-        for i in Nsize:
-            if piece[-i]:
-                cant = True
-                break
-        if cant:
+        if any(piece[-4:]):
             break
-        piece = piece[:4]
+        piece = piece[:-4]
         [piece.insert(0, False) for _ in range(Nsize)]
-        ups.append(piece)
+        ups.append(piece.copy())
     workingShifts.extend(ups)
     sides.clear()
     for spiece in ups:
         piece = spiece
-        piece : list
+        spiece : list
         # shift right
         while True:
-            cant = False
-            for i in Nsize:
-                if piece[(Nsize * i) - 1]:
-                    cant = True
-                    break
-            if cant:
+            if any(piece[Nsize-1::Nsize]):
                 break
             for line in range(Nsize):
                 piece.pop((Nsize * (line+1)) -1)
-                piece.insert(Nsize * line)
+                piece.insert(Nsize * line, False)  
+            sides.append(piece.copy())
+        piece = spiece
+        while True:
+            if any(piece[::Nsize]):
+                break
+            for line in range(Nsize):
+                piece.pop(Nsize *line)
+                piece.insert((Nsize * (line+1)) - 1, False)
+            sides.append(piece.copy())
+        workingShifts.extend(sides)
+    allpiecesList[piecenum] = list(set(tuple(i) for i in workingShifts))
 
-    
-        
+round1candidates = {key : set(allpiecesList[val]) for key, val in allpiecesList.items()}
+
+# check each possible transformed piece and see if it could be part of the bull
+
+def couldBeBull(piece: list):
+    for index, tile in enumerate(piece):
+        if tile:
+            if not bull[index]:
+                return False
+    return True
+
+round2candidates = [i for i in round1candidates if couldBeBull(i)]
+pass
 
 
