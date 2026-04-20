b1ea82d0eed243ea7f0e362b5fee8c3d4afe246f
ScientificSnake <andrew.segerlind@gmail.com>
2026-04-20 15:28:57 -0700

    HOW IS IT TOO SLOW WHAT THE HELLY

--- gfoxcp/prob14stringalchemy.py
+++ gfoxcp/prob14stringalchemy.py
@@ -2,7 +2,18 @@ import sys
 
 def main():
     lines = sys.stdin.readlines()
-    for i in lines:
-        print('b')
+    
+    for line in lines:
+        operations = 0
+
+        while True:
+            i = 0
+            k = len(line) - 1 -i
+            if line[i] == line[k]:
+                i += 1
+                continue
+            
+
+
 
 main()
